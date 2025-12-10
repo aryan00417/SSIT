90f8c65043ec58c9239e7e8ba38a37a912c29d57
ARYAN <aryan.00417@gmail.com>
2025-12-10 23:04:34 +0530

    Middlewaear to handle CORS

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv, { config } from 'dotenv'
 
-
-
+dotenv.config()
 const app = express()
+
+//middleware to handle cors
+app.use(
+  cors({
+    origin: process.env.FRONT_END_URL || "http://localhost:5173",
+    methods: ["GET","POST","PUT","DELETE"],
+    allowedHeaders: ["Content-Type","Authorization"],
+  })
+)
+
+//Middle wear to handle JSON obj in req body
+
+app.use(express.json())
+
 
 app.listen(3000,()=>{
   console.log("the server is running at port 3000!")
