05edf90a1e4493dfc40b93dfd3a600c48fe8ff00
ARYAN <aryan.00417@gmail.com>
2025-12-18 21:28:51 +0530

    get user by id api route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { adminOnly, verifyToken } from '../utils/verifyUser.js'
-import { getUsers } from '../controller/user.controller.js'
+import { getUserById, getUsers } from '../controller/user.controller.js'
 
 
 const router = express.Router()
 
 //to get user info for admin
 router.get("/get-users",verifyToken,adminOnly,getUsers)
+router.get("/:id",verifyToken,getUserById)
 
 export default router
